0e668a85fb5b12d7e10ef159caf0b6790197402d
kebertx <kebertyx@gmail.com>
2014-09-06 04:29:24 -0600

    function

--- main.lua
+++ main.lua
@@ -1,6 +1,35 @@
+function love.load()
+    world = love.physics.newWorld(0, 200, true)
+
+    ball = {}
+    ball.b = love.physics.newBody(world, 400,200, "dynamic")
+    ball.b:setMass(10)
+    ball.s = love.physics.newCircleShape(50)
+end
+
+function love.update(dt)
+    world:update(dt)
+    bd = 1
+
+    if love.keyboard.isDown(" ") then
+        a, b, c = math.random(255), math.random(255), math.random(255)
+        love.graphics.setBackgroundColor(a, b, c)
+    end
+
+    if ball.b:getY() > 500 then 
+       ball.b:applyForce(bd * 5000, -15000)
+    end
+    if ball.b:getX() < 100 then
+       bd = 1
+       ball.b:applyForce(10000, 0)
+    elseif ball.b:getX() > 500 then
+       bd = -1
+       ball.b:applyForce(-10000, 0)
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
-    love.graphics.setBackgroundColor(math.random(255), math.random(255), math.random(255))
+    love.graphics.circle("line", ball.b:getX(),ball.b:getY(), ball.s:getRadius(), 20)
 end
 
 
