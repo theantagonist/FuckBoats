bc91da1bb962f9348e16a3cda76333d215ebb2ab
theantagonist <dante@localhost.(none)>
2014-09-06 19:53:24 -0600

    dickdicksdicks

---
+++ conf.lua
@@ -0,0 +1,18 @@
+function love.conf(t)
+	t.modules.audio = true
+	t.modules.keyboard = true
+	t.modules.event = true
+	t.modules.mouse = true
+	t.modules.image = true
+	t.modules.graphics = true
+	t.modules.timer = true
+	t.modules.sound = true
+	t.modules.thread = true
+	t.console = true
+	t.screen.fullscreen = false
+	t.screen.vsync = true
+	t.title = "Fuck Boats"
+	t.author = "The Fair and Magical Jessicorn"
+	t.screen.height = 480
+	t.screen.width = 640
+end
